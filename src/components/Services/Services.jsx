eb5231968a9494b459af3bd6972d99eae79e7cf9
Mateo <mtaycruz@gmail.com>
2021-06-22 12:06:44 -0400

    all services displayed on service page

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -5,8 +5,8 @@ import './services-styling.css';
 function Services(prop){
     return(
         <div className="wrapper">
-            <section className="full-service-section">
-                <div className="full-service-details-container">
+            <section className="service-section">
+                <div className="service-details-container">
                     <div>
                         <h1>full wedding planning</h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident id ratione officiis, cumque deleniti placeat rem sint repellendus veritatis. Praesentium optio ut at ullam porro animi consequatur facere voluptas reprehenderit magni. Consequatur deserunt provident nihil officiis magni, totam molestiae repellat soluta quod, perspiciatis illo. Hic dolores molestias animi laborum corporis!</p>
@@ -46,9 +46,107 @@ function Services(prop){
                         </ul>
                     </div>
                 </div>
-                <div className="full-service-images-container">
+                <div className="service-images-container">
                     <div><img src={require("../../assets/images/Styled-28.jpg").default} alt="plates and drinking glasses layed out nicely on the table for reception"></img></div>
                     <div><img src={require("../../assets/images/Styled-59.jpg").default} alt="bride cutting the wedding cake"></img></div>
+                </div>
+            </section>
+            <div>
+                <h2>book now</h2>
+            </div>
+            <section className="service-section">
+                <div className="service-images-container">
+                    <div><img src={require("../../assets/images/Styled-44.jpg").default} alt="brides and bridesmaids cheers to good times"></img></div>
+                    <div><img src={require("../../assets/images/Styled-69.jpg").default} alt="bride and groom holding hands"></img></div>
+                </div>
+                <div>
+                    <div className="align-text-right">
+                        <h1>partial wedding planning</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolorem rerum ad blanditiis unde adipisci ipsum, natus modi sunt perferendis earum corporis veritatis maiores sed enim sint accusamus facilis eos delectus officia deserunt. Quos impedit, facilis at voluptate et amet ut, atque qui neque praesentium ea quod unde fuga incidunt.</p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, temporibus.</p>
+                            </li>
+                            <li>
+                                <p>Nulla ipsum impedit in nesciunt quo. Fugiat laborum dignissimos mollitia?</p>
+                            </li>
+                            <li>
+                                <p>Vitae temporibus architecto dolor sapiente itaque tenetur impedit optio quas!</p>
+                            </li>
+                            <li>
+                                <p>Commodi adipisci eum non dignissimos veritatis culpa eveniet nulla qui.</p>
+                            </li>
+                            <li>
+                                <p>Voluptates eveniet ullam, commodi rem possimus alias accusamus quidem voluptatum.</p>
+                            </li>
+                            <li>
+                                <p>Vel aliquam, suscipit maxime eaque quaerat nesciunt omnis itaque quos.</p>
+                            </li>
+                            <li>
+                                <p>Quisquam quibusdam et quidem porro inventore, dicta temporibus voluptas natus.</p>
+                            </li>
+                            <li>
+                                <p>Ratione ducimus eaque vero atque delectus ullam voluptates voluptas mollitia?</p>
+                            </li>
+                            <li>
+                                <p>Ullam quasi ex, necessitatibus dignissimos at nemo ipsam atque sunt?</p>
+                            </li>
+                            <li>
+                                <p>Animi laborum sit debitis laboriosam accusamus, assumenda quis error quae.</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <div>
+                <h2>book now</h2>
+            </div>
+            <section className="service-section">
+                <div>
+                    <div>
+                        <h1>Day of Coordination</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium expedita ad vero doloremque magnam veritatis odio, iusto, repellat quibusdam earum provident. Quo consequuntur possimus minima magnam nobis hic! Dolore, perferendis consequatur. Quam repellendus fugit voluptas nostrum accusamus libero optio, esse illo, minima ipsam ducimus explicabo iste veniam delectus ullam eius?</p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, aperiam.</p>
+                            </li>
+                            <li>
+                                <p>Fuga aut ipsum praesentium nobis quia quibusdam cumque delectus dolor!</p>
+                            </li>
+                            <li>
+                                <p>Nesciunt porro ad repellendus sit repellat, tempora dignissimos facere hic.</p>
+                            </li>
+                            <li>
+                                <p>Cum doloribus blanditiis impedit nesciunt sint ipsum, odit dignissimos quaerat.</p>
+                            </li>
+                            <li>
+                                <p>Eius odit at architecto neque mollitia dolorem dolor cumque nobis.</p>
+                            </li>
+                            <li>
+                                <p>Unde architecto voluptate laborum fugit officiis fugiat suscipit minima itaque?</p>
+                            </li>
+                            <li>
+                                <p>Ullam, quibusdam mollitia. Sapiente doloremque perferendis tempore quia eos molestias.</p>
+                            </li>
+                            <li>
+                                <p>Eligendi obcaecati, ullam pariatur aliquid facilis voluptatibus similique ex enim!</p>
+                            </li>
+                            <li>
+                                <p>Ducimus nam amet alias saepe dignissimos consequatur tempore vel at?</p>
+                            </li>
+                            <li>
+                                <p>Cum sit nulla aliquid, totam perspiciatis id autem animi expedita?</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="service-images-container">
+                    <div><img src={require("../../assets/images/Styled-14.jpg").default} alt="groom clipping his suspenders to his pants"></img></div>
+                    <div><img src={require("../../assets/images/Styled-5.jpg").default} alt="just the bride shoes, dress, wedding ring and crown"></img></div>
                 </div>
             </section>
             <div>
